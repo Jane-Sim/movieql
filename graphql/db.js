a7cbbfbcac860312fcb1902e3b03894c1cc29990
Sim Seyoung <ssiox3@gmail.com>
2021-01-26 01:04:37 +0900

    create movie Query and Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,22 +1,42 @@
-export const people = [
+let movies = [
   {
-    id: "0",
+    id: 0,
     name: "Seyoung",
-    age: 29,
-    gender: "female",
+    score: 1,
   },
   {
-    id: "1",
+    id: 1,
     name: "Seyoung2",
-    age: 29,
-    gender: "female",
+    score: 1,
   },
-  { id: "2", name: "Seyoung3", age: 29, gender: "female" },
-  { id: "3", name: "Seyoung4", age: 29, gender: "female" },
-  { id: "4", name: "Seyoung5", age: 29, gender: "female" },
+  { id: 2, name: "Seyoung3", score: 1 },
+  { id: 3, name: "Seyoung4", score: 1 },
+  { id: 4, name: "Seyoung5", score: 1 },
 ];
+
+export const getMovies = () => movies;
 
 export const getById = (id) => {
   const filteredPeople = people.filter((person) => person.id === String(id));
   return filteredPeople[0];
+};
+
+export const deletMovie = (id) => {
+  const cleanMovies = movies.filter((movie) => movie.id !== String(id));
+  if (movies.length > cleanMovies.length) {
+    movies = cleanMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
